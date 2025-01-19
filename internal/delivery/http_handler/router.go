9370ad694700c16b5a7eabfe7561f08d64a92bb0
indraoctama <indra.octama@smartfren.com>
2025-01-19 23:52:33 +0700

    create health handler

--- internal/delivery/http_handler/router.go
+++ internal/delivery/http_handler/router.go
@@ -2,12 +2,16 @@ package http_handler
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
 	"test-billing/commons/options"
 )
 
 func RegisterEchoRoutes(opt options.Options, handler Handler) (echoRoutes *echo.Echo) {
 	echoRoutes = echo.New()
 
+	echoRoutes.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Service is healthy!")
+	})
 	echoRoutes.POST("/loans", handler.CreateLoan)
 	echoRoutes.GET("/loans/:id/outstanding", handler.GetOutstanding)
 	echoRoutes.GET("/loans/:id/delinquent", handler.IsDelinquent)
